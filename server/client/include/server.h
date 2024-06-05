382cca4846e874ff14ba2c4c18012bd3b695c8fb
SALMANE_ATMANIA <113948229+Hisoka742@users.noreply.github.com>
2024-06-05 03:44:10 +0300

    Create server.h

---
+++ server/client/include/server.h
@@ -0,0 +1,26 @@
+#include "TaskQueue.hpp"
+#include "ThreadPool.hpp"
+#include "spdlog/spdlog.h"
+
+// Exclude <unistd.h> on Windows
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
+class Server {
+private:
+    std::shared_ptr<TaskQueue> task_queue_;
+    std::shared_ptr<ThreadPool> thread_pool_;
+    int max_threads_;
+    std::shared_ptr<spdlog::logger> logger_;
+
+public:
+    Server(int max_threads);
+    ~Server();
+
+    void start();
+    void stop();
+    void addTask(std::shared_ptr<Task> task);
+};
+
+#endif // SERVER_HPP
